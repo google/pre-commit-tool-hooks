be61fb4fc32cf8db0b06b9dafee81e03beee4de2
jonmeow <46229924+jonmeow@users.noreply.github.com>
2020-11-11 15:20:38 -0800

    Implement doc style replacements

--- pre_commit_hooks/check_google_doc_style.py
+++ pre_commit_hooks/check_google_doc_style.py
@@ -19,13 +19,32 @@ limitations under the License.
 """
 
 import argparse
-import datetime
-import os
 import re
 import sys
-import textwrap
 
-import mistune
+_IGNORE_START = "<!-- google-doc-style-ignore -->"
+_IGNORE_STOP = "<!-- google-doc-style-resume -->"
+
+# These are from https://developers.google.com/style/word-list.
+_REPLACERS = {
+    ("blacklist", "blocklist"),
+    ("cons", "disadvantages"),
+    ("e.g.", "for example"),
+    ("file name", "filename"),
+    ("filesystem", "file system"),
+    ("flag", "option"),
+    ("flags", "options"),
+    ("for instance", "for example"),
+    ("i.e.", "that is"),
+    ("pros", "advantages"),
+    ("repo", "repository"),
+    ("repos", "repositories"),
+    ("tl;dr", "to summarize"),
+    ("via", "by way of"),
+    ("vice versa", "the other way around"),
+    ("vs.", "versus"),
+    ("whitelist", "allowlist"),
+}
 
 
 def _parse_args(argv=None):
@@ -40,15 +59,60 @@ def _parse_args(argv=None):
     return parser.parse_args(args=argv)
 
 
-def _check_style(path):
+def build_replacers():
+    """Builds replacement regex objects."""
+    for capitalize in (True, False):
+        for before, after in _REPLACERS:
+            if capitalize:
+                before = before.capitalize()
+                after = after.capitalize()
+            # This needs to handle abbreviations, such as `i.e.`, and so checks
+            # for the full search word. It may be useful to optimize if this
+            # becomes a performance issue.  Use negative lookbehind and
+            # lookahead to ensure we have word breaks.
+            yield (r"(?<!\w)%s(?!\w)" % re.escape(before), after)
+
+
+def _check_style(replacers, path):
     """Checks documentation style for the given path.
 
-    Returns a list of errors, if any.
+    Returns errors, if any.
     """
-    errors = []
+    with open(path) as f:
+        contents = f.read()
 
-    md = mistune.Markdown(mistune.AstRenderer())
-    tokens = md.parse(contents)
+    lines = contents.split("\n")
+    ignoring = False
+    for index in range(len(lines)):
+        line = lines[index]
+        if line == _IGNORE_START:
+            if ignoring:
+                return "Found adjacent %s without %s on line %d" % (
+                    _IGNORE_START,
+                    _IGNORE_STOP,
+                    index + 1,
+                )
+            ignoring = True
+        elif line == _IGNORE_STOP:
+            if not ignoring:
+                return "Found %s without a preceding %s on line %d" % (
+                    _IGNORE_STOP,
+                    _IGNORE_START,
+                    index + 1,
+                )
+            ignoring = False
+        elif not ignoring:
+            new_line = line
+            for before, after in replacers:
+                new_line = re.sub(before, after, new_line)
+            lines[index] = new_line
+    if ignoring:
+        return "Found %s without a stopping %s" % (_IGNORE_START, _IGNORE_STOP)
+
+    new_contents = "\n".join(lines)
+    if new_contents != contents:
+        with open(path, "w") as f:
+            f.write(new_contents)
 
 
 def main(argv=None):
@@ -57,11 +121,14 @@ def main(argv=None):
     parsed_args = _parse_args(argv[1:])
     paths = parsed_args.paths
 
+    # Build the list of replacer regexes once, and re-use it for all files.
+    replacers = list(build_replacers())
+
     exit_code = 0
     for path in paths:
         if not path.endswith(".md"):
             continue
-        errors = _check_style(path)
+        errors = _check_style(replacers, path)
         if errors:
             print("Errors in `%s`: %s" % (path, errors))
             exit_code = 1
---
+++ pre_commit_hooks/check_google_doc_style_test.py
@@ -0,0 +1,112 @@
+"""Tests for check_google_doc_style.py."""
+
+__copyright__ = """
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import tempfile
+import unittest
+
+from pre_commit_hooks import check_google_doc_style
+
+
+class TestCheckGoogleDocStyle(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = 1000
+
+    def _assert_contents(self, orig, expected, ext=".md", exit_code=0):
+        with tempfile.NamedTemporaryFile(
+            suffix=ext, mode="w", delete=False
+        ) as f:
+            filename = f.name
+            f.write(orig)
+            f.flush()
+
+        self.assertEqual(
+            check_google_doc_style.main(argv=["bin", filename]), exit_code
+        )
+        with open(filename) as f:
+            self.assertEqual(f.read(), expected)
+
+        os.unlink(filename)
+
+    def test_wrong_ext(self):
+        contents = "Something\n\ncons:\n\n- Example"
+        self._assert_contents(contents, contents, ext=".py")
+
+    def test_noop(self):
+        contents = "Do nothing."
+        self._assert_contents(contents, contents)
+
+    def test_basic(self):
+        before = "Something\n\ncons:\n\n- Example"
+        after = "Something\n\ndisadvantages:\n\n- Example"
+        self._assert_contents(before, after)
+
+    def test_basic_capitalize(self):
+        before = "Something\n\nCons:\n\n- Example"
+        after = "Something\n\nDisadvantages:\n\n- Example"
+        self._assert_contents(before, after)
+
+    def test_repeated(self):
+        before = "cons cons cons"
+        after = "disadvantages disadvantages disadvantages"
+        self._assert_contents(before, after)
+
+    def test_abbreviation(self):
+        before = "Programming languages, e.g., C++"
+        after = "Programming languages, for example, C++"
+        self._assert_contents(before, after)
+
+    def test_abbreviation_capitalize(self):
+        before = "Programming languages. E.g., C++"
+        after = "Programming languages. For example, C++"
+        self._assert_contents(before, after)
+
+    def test_ignore(self):
+        before = (
+            "Cons\n"
+            "<!-- google-doc-style-ignore -->\n"
+            "Cons\n"
+            "<!-- google-doc-style-resume -->\n"
+            "Cons"
+        )
+        after = (
+            "Disadvantages\n"
+            "<!-- google-doc-style-ignore -->\n"
+            "Cons\n"
+            "<!-- google-doc-style-resume -->\n"
+            "Disadvantages"
+        )
+        self._assert_contents(before, after)
+
+    def test_ignore_missing_start(self):
+        contents = "Cons\n<!-- google-doc-style-resume -->\nCons"
+        self._assert_contents(contents, contents, exit_code=1)
+
+    def test_ignore_missing_end(self):
+        contents = "Cons\n<!-- google-doc-style-ignore -->\nCons"
+        self._assert_contents(contents, contents, exit_code=1)
+
+    def test_ignore_repeated(self):
+        contents = (
+            "Cons\n"
+            "<!-- google-doc-style-ignore -->\n"
+            "<!-- google-doc-style-ignore -->\n"
+            "<!-- google-doc-style-resume -->\n"
+            "Cons"
+        )
+        self._assert_contents(contents, contents, exit_code=1)
